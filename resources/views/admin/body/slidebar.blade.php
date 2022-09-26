fcae060d70bef26eadbbb64158c4926c17da5c4b
An <dacn1113@gmail.com>
2022-09-26 13:43:23 +0700

    Khu vực và vận chuyển

--- resources/views/admin/body/slidebar.blade.php
+++ resources/views/admin/body/slidebar.blade.php
@@ -1,10 +1,11 @@
 @php
-$prefix = Request::route()->getPrefix();
-$route = Route::current()->getName();
-
+  $prefix = Request::route()->getPrefix();
+  $route = Route::current()->getName();
+ 
 @endphp
 
-<aside class="main-sidebar">
+
+  <aside class="main-sidebar">
     <!-- sidebar-->
     <section class="sidebar">	
 		
@@ -13,8 +14,8 @@ $route = Route::current()->getName();
 				 <a href="index.html">
 				  <!-- logo for regular state and mobile devices -->
 					 <div class="d-flex align-items-center justify-content-center">					 	
-						  <img src="{{asset('backend/images/logo-dark.png')}}" alt="">
-						  <h3><b>Sunny</b> Shop</h3>
+						  <img src="{{ asset('backend/images/logo-dark.png') }}" alt="">
+						  <h3><b>Easy</b> Shop</h3>
 					 </div>
 				</a>
 			</div>
@@ -23,40 +24,41 @@ $route = Route::current()->getName();
       <!-- sidebar menu-->
       <ul class="sidebar-menu" data-widget="tree">  
 		  
-		<li class="{{ ($route == 'dashboard')? 'active':''}}">
-          <a href="{{url('admin/dashboard')}}">
+		<li class="{{ ($route == 'dashboard')? 'active':'' }}">
+          <a href="{{ url('admin/dashboard') }}">
             <i data-feather="pie-chart"></i>
 			<span>Dashboard</span>
           </a>
         </li>  
 		
-        <li class="treeview {{ ($prefix == '/brand')?'active':''}}">
+        <li class="treeview {{ ($prefix == '/brand')?'active':'' }}  ">
           <a href="#">
             <i data-feather="message-circle"></i>
-            <span>Brand</span>
+            <span>Brands</span>
             <span class="pull-right-container">
               <i class="fa fa-angle-right pull-right"></i>
             </span>
           </a>
           <ul class="treeview-menu">
-            <li class="{{ ($route == 'all.brand')?'active':''}}"><a href="{{route('all.brand')}}"><i class="ti-more"></i>All Brand</a></li>
-   
+            <li class="{{ ($route == 'all.brand')? 'active':'' }}"><a href="{{ route('all.brand') }}"><i class="ti-more"></i>All Brand</a></li>
+            
           </ul>
         </li> 
 		  
-        <li class="treeview {{ ($prefix == '/category')?'active':''}}">
+        <li class="treeview {{ ($prefix == '/category')?'active':'' }}  ">
           <a href="#">
-            <i data-feather="mail"></i> <span>Category</span>
+            <i data-feather="mail"></i> <span>Category </span>
             <span class="pull-right-container">
               <i class="fa fa-angle-right pull-right"></i>
             </span>
           </a>
           <ul class="treeview-menu">
-            <li class="{{ ($route == 'all.category')? 'active':'' }}"><a href="{{ route('all.category') }}"><i class="ti-more"></i>All Category</a></li>
-            <li class="{{ ($route == 'all.subcategory')? 'active':'' }}"><a href="{{ route('all.subcategory') }}"><i class="ti-more"></i>All SubCategory</a></li>
-            <li class="{{ ($route == 'all.subsubcategory')? 'active':'' }}"><a href="{{ route('all.subsubcategory') }}"><i class="ti-more"></i>All Sub->SubCategory</a></li>
+         <li class="{{ ($route == 'all.category')? 'active':'' }}"><a href="{{ route('all.category') }}"><i class="ti-more"></i>All Category</a></li>
+         <li class="{{ ($route == 'all.subcategory')? 'active':'' }}"><a href="{{ route('all.subcategory') }}"><i class="ti-more"></i>All SubCategory</a></li>
+<li class="{{ ($route == 'all.subsubcategory')? 'active':'' }}"><a href="{{ route('all.subsubcategory') }}"><i class="ti-more"></i>All Sub->SubCategory</a></li>
 
-          </ul>
+
+                      </ul>
         </li>
 		
         <li class="treeview {{ ($prefix == '/product')?'active':'' }}  ">
@@ -70,15 +72,15 @@ $route = Route::current()->getName();
           <ul class="treeview-menu">
             <li class="{{ ($route == 'add-product')? 'active':'' }}"><a href="{{ route('add-product') }}"><i class="ti-more"></i>Add Products</a></li>
 
-            <li class="{{ ($route == 'manage-product')? 'active':'' }}"><a href="{{ route('manage-product') }}"><i class="ti-more"></i>Manage Products</a></li>
-
+             <li class="{{ ($route == 'manage-product')? 'active':'' }}"><a href="{{ route('manage-product') }}"><i class="ti-more"></i>Manage Products</a></li>
+             
           </ul>
         </li> 		  
-		 
-        <li class="header nav-small-cap">User Interface</li>
-		  
 
-        <li class="treeview {{ ($prefix == '/slider')?'active':'' }}  ">
+
+
+
+         <li class="treeview {{ ($prefix == '/slider')?'active':'' }}  ">
           <a href="#">
             <i data-feather="file"></i>
             <span>Slider</span>
@@ -89,10 +91,57 @@ $route = Route::current()->getName();
           <ul class="treeview-menu">
             <li class="{{ ($route == 'manage-slider')? 'active':'' }}"><a href="{{ route('manage-slider') }}"><i class="ti-more"></i>Manage Slider</a></li>
 
+             
+             
+          </ul>
+        </li>   
 
 
+
+         <li class="treeview {{ ($prefix == '/coupons')?'active':'' }}  ">
+          <a href="#">
+            <i data-feather="file"></i>
+            <span>Coupons</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-right pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li class="{{ ($route == 'manage-coupon')? 'active':'' }}"><a href="{{ route('manage-coupon') }}"><i class="ti-more"></i>Manage Coupon</a></li>
+
+             
+             
           </ul>
         </li>      
+
+
+
+         <li class="treeview {{ ($prefix == '/shipping')?'active':'' }}  ">
+          <a href="#">
+            <i data-feather="file"></i>
+            <span>Shipping Area</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-right pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+        <li class="{{ ($route == 'manage-division')? 'active':'' }}"><a href="{{ route('manage-division') }}"><i class="ti-more"></i>Ship Division</a></li>
+
+         <li class="{{ ($route == 'manage-district')? 'active':'' }}"><a href="{{ route('manage-district') }}"><i class="ti-more"></i>Ship District</a></li>
+
+         <li class="{{ ($route == 'manage-state')? 'active':'' }}"><a href="{{ route('manage-state') }}"><i class="ti-more"></i>Ship State</a></li>
+
+             
+             
+          </ul>
+        </li>         
+
+
+
+
+		 
+        <li class="header nav-small-cap">User Interface</li>
+		  
         <li class="treeview">
           <a href="#">
             <i data-feather="grid"></i>
@@ -104,78 +153,29 @@ $route = Route::current()->getName();
           <ul class="treeview-menu">
             <li><a href="components_alerts.html"><i class="ti-more"></i>Alerts</a></li>
             <li><a href="components_badges.html"><i class="ti-more"></i>Badge</a></li>
-            <li><a href="components_buttons.html"><i class="ti-more"></i>Buttons</a></li>
-            <li><a href="components_sliders.html"><i class="ti-more"></i>Sliders</a></li>
-            <li><a href="components_dropdown.html"><i class="ti-more"></i>Dropdown</a></li>
-            <li><a href="components_modals.html"><i class="ti-more"></i>Modal</a></li>
-            <li><a href="components_nestable.html"><i class="ti-more"></i>Nestable</a></li>
-            <li><a href="components_progress_bars.html"><i class="ti-more"></i>Progress Bars</a></li>
+      <li><a href="components_buttons.html"><i class="ti-more"></i>Buttons</a></li>
+            
           </ul>
         </li>
-      </li>   
-
-
-
-      <li class="treeview {{ ($prefix == '/coupons')?'active':'' }}  ">
-       <a href="#">
-         <i data-feather="file"></i>
-         <span>Coupons</span>
-         <span class="pull-right-container">
-           <i class="fa fa-angle-right pull-right"></i>
-         </span>
-       </a>
-       <ul class="treeview-menu">
-         <li class="{{ ($route == 'manage-coupon')? 'active':'' }}"><a href="{{ route('manage-coupon') }}"><i class="ti-more"></i>Manage Coupon</a></li>
-
-
-
-       </ul>
-     </li>         
 		
-		  
-		<li class="header nav-small-cap">EXTRA</li>		  
-		  
-        <li class="treeview">
+		<li class="treeview">
           <a href="#">
-            <i data-feather="layers"></i>
-			<span>Multilevel</span>
+            <i data-feather="credit-card"></i>
+            <span>Cards</span>
             <span class="pull-right-container">
               <i class="fa fa-angle-right pull-right"></i>
             </span>
           </a>
           <ul class="treeview-menu">
-            <li><a href="#">Level One</a></li>
-            <li class="treeview">
-              <a href="#">Level One
-                <span class="pull-right-container">
-              <i class="fa fa-angle-right pull-right"></i>
-            </span>
-              </a>
-              <ul class="treeview-menu">
-                <li><a href="#">Level Two</a></li>
-                <li class="treeview">
-                  <a href="#">Level Two
-                    <span class="pull-right-container">
-					  <i class="fa fa-angle-right pull-right"></i>
-					</span>
-                  </a>
-                  <ul class="treeview-menu">
-                    <li><a href="#">Level Three</a></li>
-                    <li><a href="#">Level Three</a></li>
-                  </ul>
-                </li>
-              </ul>
-            </li>
-            <li><a href="#">Level One</a></li>
-          </ul>
+			<li><a href="card_advanced.html"><i class="ti-more"></i>Advanced Cards</a></li>
+			<li><a href="card_basic.html"><i class="ti-more"></i>Basic Cards</a></li>
+			<li><a href="card_color.html"><i class="ti-more"></i>Cards Color</a></li>
+		  </ul>
         </li>  
 		  
-		<li>
-          <a href="auth_login.html">
-            <i data-feather="lock"></i>
-			<span>Log Out</span>
-          </a>
-        </li> 
+        		  
+		  
+		 
         
       </ul>
     </section>
--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,24 @@
 <?php
 
+use App\Models\User;
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
-use App\Http\Controllers\Backend\AdminprofileController;
+use App\Http\Controllers\Backend\AdminProfileController;
 use App\Http\Controllers\Backend\BrandController;
 use App\Http\Controllers\Backend\CategoryController;
-use App\Http\Controllers\Backend\CouponController;
+use App\Http\Controllers\Backend\SubCategoryController;
 use App\Http\Controllers\Backend\ProductController;
 use App\Http\Controllers\Backend\SliderController;
-use App\Http\Controllers\Backend\SubCategoryController;
+use App\Http\Controllers\Backend\CouponController;
+use App\Http\Controllers\Backend\ShippingAreaController;
+
 use App\Http\Controllers\Frontend\IndexController;
 use App\Http\Controllers\Frontend\LanguageController;
-use Illuminate\Support\Facades\Auth;
-use app\Models\User;
 use App\Http\Controllers\Frontend\CartController;
-use App\Http\Controllers\User\CartPageController;
-use App\Http\Controllers\User\WishlistController;
 
+use App\Http\Controllers\User\WishlistController;
+use App\Http\Controllers\User\CartPageController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -28,109 +30,136 @@ use App\Http\Controllers\User\WishlistController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::group(['prefix' => 'admin', 'middleware' => ['admin:admin']], function () {
     Route::get('/login', [AdminController::class, 'loginForm']);
     Route::post('/login', [AdminController::class, 'store'])->name('admin.login');
 });
+
+
 Route::middleware(['auth:admin'])->group(function () {
+
+
 
     Route::middleware(['auth:sanctum,admin', 'verified'])->get('/admin/dashboard', function () {
         return view('admin.index');
     })->name('dashboard')->middleware('auth:admin');
 
-    // Route::middleware([
-    //     'auth:sanctum', config('jetstream.auth_session'), 'verified'
-    // ])->group(function () {
-    //     Route::get('/dashboard', function () {
-    //         return view('admin.index');
-    //     })->name('dashboard');
-    // });
+    // Admin All Routes 
 
-    //Admin All routes
     Route::get('/admin/logout', [AdminController::class, 'destroy'])->name('admin.logout');
-    Route::get('/admin/profile', [AdminprofileController::class, 'AdminProfile'])->name('admin.profile');
-    Route::get('/admin/profile/edit', [AdminprofileController::class, 'AdminProfileEdit'])->name('admin.profile.edit');
-    Route::post('/admin/profile/store', [AdminprofileController::class, 'AdminProfileStore'])->name('admin.profile.store');
-    Route::get('/admin/change/password', [AdminprofileController::class, 'AdminChangePassword'])->name('admin.change.password');
-    Route::post('/update/change/password', [AdminprofileController::class, 'AdminUpdateChangePassword'])->name('update.change.password');
-});
-//User All routes
+
+    Route::get('/admin/profile', [AdminProfileController::class, 'AdminProfile'])->name('admin.profile');
+
+    Route::get('/admin/profile/edit', [AdminProfileController::class, 'AdminProfileEdit'])->name('admin.profile.edit');
+
+    Route::post('/admin/profile/store', [AdminProfileController::class, 'AdminProfileStore'])->name('admin.profile.store');
+
+    Route::get('/admin/change/password', [AdminProfileController::class, 'AdminChangePassword'])->name('admin.change.password');
+
+    Route::post('/update/change/password', [AdminProfileController::class, 'AdminUpdateChangePassword'])->name('update.change.password');
+});  // end Middleware admin
+
+// User ALL Routes
+
 Route::middleware(['auth:sanctum,web', 'verified'])->get('/dashboard', function () {
     $id = Auth::user()->id;
     $user = User::find($id);
     return view('dashboard', compact('user'));
 })->name('dashboard');
+
 Route::get('/', [IndexController::class, 'index']);
 Route::get('/user/logout', [IndexController::class, 'UserLogout'])->name('user.logout');
+
 Route::get('/user/profile', [IndexController::class, 'UserProfile'])->name('user.profile');
+
 Route::post('/user/profile/store', [IndexController::class, 'UserProfileStore'])->name('user.profile.store');
+
 Route::get('/user/change/password', [IndexController::class, 'UserChangePassword'])->name('change.password');
+
 Route::post('/user/password/update', [IndexController::class, 'UserPasswordUpdate'])->name('user.password.update');
 
-//Admin brand all
 
-route::prefix('brand')->group(
-    function () {
-        Route::get('/view', [BrandController::class, 'BrandView'])->name('all.brand');
-        Route::post('/store', [BrandController::class, 'BrandStore'])->name('brand.store');
-        Route::get('/edit/{id}', [BrandController::class, 'BrandEdit'])->name('brand.edit');
-        Route::post('/update', [BrandController::class, 'BrandUpdate'])->name('brand.update');
-        Route::get('/delete/{id}', [BrandController::class, 'BrandDelete'])->name('brand.delete');
-    }
-);
+// Admin Brand All Routes 
 
-//Admin category all
+Route::prefix('brand')->group(function () {
 
-route::prefix('category')->group(
-    function () {
-        Route::get('/view', [CategoryController::class, 'CategoryView'])->name('all.category');
-        Route::post('/store', [CategoryController::class, 'CategoryStore'])->name('category.store');
-        Route::get('/edit/{id}', [CategoryController::class, 'CategoryEdit'])->name('category.edit');
-        Route::post('/update', [CategoryController::class, 'CategoryUpdate'])->name('category.update');
-        Route::get('/delete/{id}', [CategoryController::class, 'CategoryDelete'])->name('category.delete');
+    Route::get('/view', [BrandController::class, 'BrandView'])->name('all.brand');
 
-        // Admin Sub Category All Routes
+    Route::post('/store', [BrandController::class, 'BrandStore'])->name('brand.store');
 
-        Route::get('/sub/view', [SubCategoryController::class, 'SubCategoryView'])->name('all.subcategory');
+    Route::get('/edit/{id}', [BrandController::class, 'BrandEdit'])->name('brand.edit');
 
-        Route::post('/sub/store', [SubCategoryController::class, 'SubCategoryStore'])->name('subcategory.store');
+    Route::post('/update', [BrandController::class, 'BrandUpdate'])->name('brand.update');
 
-        Route::get('/sub/edit/{id}', [SubCategoryController::class, 'SubCategoryEdit'])->name('subcategory.edit');
+    Route::get('/delete/{id}', [BrandController::class, 'BrandDelete'])->name('brand.delete');
+});
 
-        Route::post('/update/{id}', [CategoryController::class, 'CategoryUpdate'])->name('category.update');
+// Admin Category all Routes  
+Route::prefix('category')->group(function () {
 
-        Route::get('/sub/delete/{id}', [SubCategoryController::class, 'SubCategoryDelete'])->name('subcategory.delete');
+    Route::get('/view', [CategoryController::class, 'CategoryView'])->name('all.category');
 
-        // Admin Sub->Sub Category All Routes
-        Route::get('/sub/sub/view', [SubCategoryController::class, 'SubSubCategoryView'])->name('all.subsubcategory');
-        Route::get('/subcategory/ajax/{category_id}', [SubCategoryController::class, 'GetSubCategory']);
-        Route::get('/sub-subcategory/ajax/{subcategory_id}', [SubCategoryController::class, 'GetSubSubCategory']);
-        Route::post('/sub/sub/store', [SubCategoryController::class, 'SubSubCategoryStore'])->name('subsubcategory.store');
-        Route::get('/sub/sub/edit/{id}', [SubCategoryController::class, 'SubSubCategoryEdit'])->name('subsubcategory.edit');
+    Route::post('/store', [CategoryController::class, 'CategoryStore'])->name('category.store');
 
-        Route::post('/sub/update', [SubCategoryController::class, 'SubSubCategoryUpdate'])->name('subsubcategory.update');
+    Route::get('/edit/{id}', [CategoryController::class, 'CategoryEdit'])->name('category.edit');
 
-        Route::get('/sub/sub/delete/{id}', [SubCategoryController::class, 'SubSubCategoryDelete'])->name('subsubcategory.delete');
-    }
-);
+    Route::post('/update/{id}', [CategoryController::class, 'CategoryUpdate'])->name('category.update');
+
+    Route::get('/delete/{id}', [CategoryController::class, 'CategoryDelete'])->name('category.delete');
+
+    // Admin Sub Category All Routes
+
+    Route::get('/sub/view', [SubCategoryController::class, 'SubCategoryView'])->name('all.subcategory');
+
+    Route::post('/sub/store', [SubCategoryController::class, 'SubCategoryStore'])->name('subcategory.store');
+
+    Route::get('/sub/edit/{id}', [SubCategoryController::class, 'SubCategoryEdit'])->name('subcategory.edit');
+
+    Route::post('/update', [SubCategoryController::class, 'SubCategoryUpdate'])->name('subcategory.update');
+
+    Route::get('/sub/delete/{id}', [SubCategoryController::class, 'SubCategoryDelete'])->name('subcategory.delete');
+
+
+    // Admin Sub->Sub Category All Routes
+
+    Route::get('/sub/sub/view', [SubCategoryController::class, 'SubSubCategoryView'])->name('all.subsubcategory');
+
+    Route::get('/subcategory/ajax/{category_id}', [SubCategoryController::class, 'GetSubCategory']);
+
+    Route::get('/sub-subcategory/ajax/{subcategory_id}', [SubCategoryController::class, 'GetSubSubCategory']);
+
+    Route::post('/sub/sub/store', [SubCategoryController::class, 'SubSubCategoryStore'])->name('subsubcategory.store');
+
+    Route::get('/sub/sub/edit/{id}', [SubCategoryController::class, 'SubSubCategoryEdit'])->name('subsubcategory.edit');
+
+    Route::post('/sub/update', [SubCategoryController::class, 'SubSubCategoryUpdate'])->name('subsubcategory.update');
+
+    Route::get('/sub/sub/delete/{id}', [SubCategoryController::class, 'SubSubCategoryDelete'])->name('subsubcategory.delete');
+});
+
+// Admin Products All Routes 
 
 Route::prefix('product')->group(function () {
+
     Route::get('/add', [ProductController::class, 'AddProduct'])->name('add-product');
 
     Route::post('/store', [ProductController::class, 'StoreProduct'])->name('product-store');
     Route::get('/manage', [ProductController::class, 'ManageProduct'])->name('manage-product');
+
     Route::get('/edit/{id}', [ProductController::class, 'EditProduct'])->name('product.edit');
+
     Route::post('/data/update', [ProductController::class, 'ProductDataUpdate'])->name('product-update');
 
     Route::post('/image/update', [ProductController::class, 'MultiImageUpdate'])->name('update-product-image');
+
+    Route::post('/thambnail/update', [ProductController::class, 'ThambnailImageUpdate'])->name('update-product-thambnail');
+
     Route::get('/multiimg/delete/{id}', [ProductController::class, 'MultiImageDelete'])->name('product.multiimg.delete');
+
     Route::get('/inactive/{id}', [ProductController::class, 'ProductInactive'])->name('product.inactive');
 
     Route::get('/active/{id}', [ProductController::class, 'ProductActive'])->name('product.active');
+
     Route::get('/delete/{id}', [ProductController::class, 'ProductDelete'])->name('product.delete');
 });
 
@@ -177,6 +206,7 @@ Route::get('/subcategory/product/{subcat_id}/{slug}', [IndexController::class, '
 // Frontend Sub-SubCategory wise Data
 Route::get('/subsubcategory/product/{subsubcat_id}/{slug}', [IndexController::class, 'SubSubCatWiseProduct']);
 
+
 // Product View Modal with Ajax
 Route::get('/product/view/modal/{id}', [IndexController::class, 'ProductViewAjax']);
 
@@ -192,13 +222,19 @@ Route::get('/minicart/product-remove/{rowId}', [CartController::class, 'RemoveMi
 // Add to Wishlist
 Route::post('/add-to-wishlist/{product_id}', [CartController::class, 'AddToWishlist']);
 
-// Wishlist page
+
+
 Route::group(['prefix' => 'user', 'middleware' => ['user', 'auth'], 'namespace' => 'User'], function () {
 
+    // Wishlist page
     Route::get('/wishlist', [WishlistController::class, 'ViewWishlist'])->name('wishlist');
+
     Route::get('/get-wishlist-product', [WishlistController::class, 'GetWishlistProduct']);
+
     Route::get('/wishlist-remove/{id}', [WishlistController::class, 'RemoveWishlistProduct']);
 });
+
+
 
 // My Cart Page All Routes
 Route::get('/mycart', [CartPageController::class, 'MyCart'])->name('mycart');
@@ -217,10 +253,53 @@ Route::get('/cart-decrement/{rowId}', [CartPageController::class, 'CartDecrement
 Route::prefix('coupons')->group(function () {
 
     Route::get('/view', [CouponController::class, 'CouponView'])->name('manage-coupon');
+
     Route::post('/store', [CouponController::class, 'CouponStore'])->name('coupon.store');
 
     Route::get('/edit/{id}', [CouponController::class, 'CouponEdit'])->name('coupon.edit');
     Route::post('/update/{id}', [CouponController::class, 'CouponUpdate'])->name('coupon.update');
 
     Route::get('/delete/{id}', [CouponController::class, 'CouponDelete'])->name('coupon.delete');
+});
+
+
+// Admin Shipping All Routes 
+
+Route::prefix('shipping')->group(function () {
+
+    // Ship Division 
+    Route::get('/division/view', [ShippingAreaController::class, 'DivisionView'])->name('manage-division');
+
+    Route::post('/division/store', [ShippingAreaController::class, 'DivisionStore'])->name('division.store');
+
+    Route::get('/division/edit/{id}', [ShippingAreaController::class, 'DivisionEdit'])->name('division.edit');
+
+    Route::post('/division/update/{id}', [ShippingAreaController::class, 'DivisionUpdate'])->name('division.update');
+
+    Route::get('/division/delete/{id}', [ShippingAreaController::class, 'DivisionDelete'])->name('division.delete');
+
+
+
+    // Ship District 
+    Route::get('/district/view', [ShippingAreaController::class, 'DistrictView'])->name('manage-district');
+
+    Route::post('/district/store', [ShippingAreaController::class, 'DistrictStore'])->name('district.store');
+
+    Route::get('/district/edit/{id}', [ShippingAreaController::class, 'DistrictEdit'])->name('district.edit');
+
+    Route::post('/district/update/{id}', [ShippingAreaController::class, 'DistrictUpdate'])->name('district.update');
+
+    Route::get('/district/delete/{id}', [ShippingAreaController::class, 'DistrictDelete'])->name('district.delete');
+
+
+    // Ship State 
+    Route::get('/state/view', [ShippingAreaController::class, 'StateView'])->name('manage-state');
+
+    Route::post('/state/store', [ShippingAreaController::class, 'StateStore'])->name('state.store');
+
+    Route::get('/state/edit/{id}', [ShippingAreaController::class, 'StateEdit'])->name('state.edit');
+
+    Route::post('/state/update/{id}', [ShippingAreaController::class, 'StateUpdate'])->name('state.update');
+
+    Route::get('/state/delete/{id}', [ShippingAreaController::class, 'StateDelete'])->name('state.delete');
 });
